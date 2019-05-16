02ab6442d5bfda2316811c0fbb1c50a7447fc926
kosho75 <kosho75@gmail.com>
2019-05-16 16:56:04 -0700

    complete tutorial

---
+++ I AM Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by 橋本 翔子 on 2018/11/29.
+//  Copyright © 2018 Shoko Hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
